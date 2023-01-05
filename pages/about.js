6cce0fd0b159446bed52398680fc215ab0767109
Raymond Hawthorne <raymondh@wusf.org>
2023-01-05 09:51:49 -0500

    Add About Us Page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,53 @@
-import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import { useEffect } from 'react'
 
-export default function About() {
+import styles from '../styles/Home.module.css'
+import stylesAbout from '../styles/About.module.css'
+
+export default function About({ about }) {
+    useEffect(() => {
+        const imgHidden = document.querySelector('.image-wrap')
+        imgHidden.style.cssText += `
+            display: none;
+        `
+
+        const fullWidths = document.querySelectorAll('.tf_box')
+        fullWidths.forEach(fullWidth => {
+            fullWidth.style.cssText += `
+                width: 100% !important;
+            `
+        })
+
+        const hostPictures = document.querySelectorAll('.tb_text_wrap > p > img')
+        hostPictures.forEach(hostPicture => {
+            hostPicture.style.cssText += `
+            float: left;
+            margin-inline-end: 1rem;
+        `
+        })
+        
+    }, [])
+
+    const title = about.title.rendered
     return (
-        <div>
-            <h1 className={styles.title}>About Page</h1>
-        </div>
+        <>
+            <Head><title>{`${title} - The Zest Podcast`}</title></Head>
+            <main className={`${styles.container__page} ${styles.container}`}>
+                <h1 className={styles.title}>{title}</h1>
+                <div className={stylesAbout.hostname} dangerouslySetInnerHTML={{__html: about.content.rendered}}></div>
+            </main>
+        </>
     )
+}
+
+export async function getStaticProps() {
+    const res = await fetch('https://thezestpodcast.com/wp-json/wp/v2/pages/507')
+    const about = await res.json()
+
+    return {
+        props: {
+            about
+        },
+        revalidate: 60
+    }
 }
